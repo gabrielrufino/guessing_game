a8b433c44d26e17c459a8d990c069941d0f59a91
Gabriel Rufino <contato@gabrielrufino.com>
2022-04-22 17:17:18 -0300

    Final version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!\n");
+
+    let secret_number = rand::thread_rng()
+        .gen_range(1..101);
+
+    loop {
+        print!("Please input your guess: ");
+        io::stdout()
+            .flush()
+            .unwrap();
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Greater => println!("Too big!\n"),
+            Ordering::Equal => {
+                println!("You win!\n");
+                break;
+            }
+        }
+    }
 }
